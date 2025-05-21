b10ff02c90fe081dd3ea17c5244f2faffed923a3
sahnia3 <123416200+sahnia3@users.noreply.github.com>
2025-05-21 19:47:00 -0400

    Update page.tsx

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,6 +1,4 @@
-"use client";
-import React from 'react';
-import { motion } from 'framer-motion';
+import { motion } from "framer-motion";
 
 const BlogPage = () => {
   return (
@@ -14,13 +12,16 @@ const BlogPage = () => {
         >
           <h1 className="text-5xl font-bold mb-6">Blog Coming Soon</h1>
           <p className="text-gray-400 max-w-2xl mx-auto">
-            I'm working on some interesting projects/articles, Stay tuned for insightful content!
+            I&apos;m working on some interesting projects/articles, Stay tuned for insightful content!
           </p>
         </motion.div>
 
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
           {[1, 2, 3].map((i) => (
-            <div key={i} className="bg-[#1B1B1B] border-2 border-[#5656578b] rounded-lg p-6 shadow-[inset_0_0_10px_rgba(70,70,70,0.5)]">
+            <div
+              key={i}
+              className="bg-[#1B1B1B] border-2 border-[#5656578b] rounded-lg p-6 shadow-[inset_0_0_10px_rgba(70,70,70,0.5)]"
+            >
               <div className="w-full h-48 bg-[#141414] rounded-lg mb-4"></div>
               <div className="h-6 w-3/4 bg-[#141414] rounded mb-3"></div>
               <div className="h-4 w-1/2 bg-[#141414] rounded"></div>
