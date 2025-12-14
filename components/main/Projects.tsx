ccdec016059463f59d49d802a9520a74b9541e33
sahnia3 <123416200+sahnia3@users.noreply.github.com>
2025-12-14 03:23:33 -0500

    Adjust padding in Projects component layout

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -10,7 +10,7 @@ const Projects = () => {
       <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
         My Projects
       </h1>
-      <div className="h-full w-full flex flex-col md:flex-row gap-20 px-20 justify-center items-stretch">
+      <div className="h-full w-full flex flex-col md:flex-row gap-20 px-6 md:px-20 justify-center items-stretch">
         <ProjectCard
           src="/axesouar.png"
           title="Axesouar.in"
