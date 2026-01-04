54483dbf6a2f9f65284b7729f576a7815e01922c
Aditya Sahni <123416200+sahnia3@users.noreply.github.com>
2026-01-04 07:14:43 -0500

    Fix video className order for Hero component

--- components/main/Hero.tsx
+++ components/main/Hero.tsx
@@ -12,11 +12,11 @@ const Hero = () => {
           autoPlay
           muted
           loop
-          className="rotate-180 absolute top-[-340px]  h-full w-full left-0 z-[1] object-cover "
+          className="rotate-180 absolute top-[-340px] left-0 z-[1] w-full h-full object-cover"
         >
           <source src="/blackhole.webm" type="video/webm" />
         </video>
-        
+
       </div>
     </>
   );
